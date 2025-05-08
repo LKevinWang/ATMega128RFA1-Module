b964d20be2a7c7ff8a211683b830d39d5e99181a
LKevinWang <1148394218@qq.com>
2025-05-08 17:54:38 +0800

    Initial Version of ASMs

--- src/Mega128_main_proc.asm
+++ src/Mega128_main_proc.asm
@@ -1,2 +1,156 @@
 .org 0x0000
+STARTPOINT:
+rjmp Initial
+
+.org 0x0120;Skip Interrupt
+
+
+Initial:
+clr r1
+out sreg, r1 ;Clear Sreg
+
+ldi r28, 0xff
+ldi r29, 0x41
+out SPH, r29
+out SPL, r28
+
+ldi r17, 0x02 
+ldi r26, 0x00 
+ldi r27, 0x02 ;X = 512
+ldi r30, 0x78 
+ldi r31, 0x07 ;Z = 1912 ????
+ldi r16, 0x00
+out RAMPZ, r16; Clear Z high2bits
+
+;Block as memcpy(512++, 1912++, 0)
+rjmp if_X_EQ_512
+LOOP_X_EQ_512:
+elpm r0, Z+
+st X+, r0
+if_X_EQ_512:
+cpi r26, 0x00
+cpc r27, r17
+brbc 1, LOOP_X_EQ_512;while x != 512
+
+;[512:518] == 0x00
+ldi r18, 0x02
+ldi r26, 0x00
+ldi r27, 0x02;X = 512
+rjmp if_X_EQ_518
+LOOP_X_EQ_518:
+st X+, r1
+if_X_EQ_518:
+cpi r26, 0x06
+cpc r27, r18
+brbc 1, LOOP_X_EQ_518
+
+rcall main
+
+rjmp ENDPOINT
+rjmp STARTPOINT
+
+main:
+push r28
+push r29
+in r28, SPL
+in r29, SPH
+rcall GPIO_Init
+rcall CLK_init
+;rcall SPI_IO_Init
+rcall RF_Init
+
+    SPI_Enable:
+    ldi r18, 0x4C
+    ldi r19, 0x00;
+    movw r30, r18
+    ld r18, Z; r18 = SPCR
+    ori r18, 0x40;
+    st Z, r18;SPCR.SPE = 1
+
+    Intps_Init:
+
+    ldi r24, 0x3c
+    ldi r25, 0x00
+    ldi r18, 0xff
+    movw r30, r24
+    st Z, r18   ;EIFR = 0xFF
+
+    ldi r18, 0x3d
+    ldi r19, 0x00
+    movw r30, r18
+    ld r18, Z
+    ori r18, 0x01
+    st Z, r18   ;EIMSK.INT0 = 1
+
+    ldi r18, 0x69
+    ldi r19, 0x00
+    movw r30, r18
+    ld r18, Z
+    ori r18, 0x03
+    st Z, r18   ;EICRA.[ICS00:ICS01] = 1
+    bset 7;Enable Global Interrupt
+
+    Wait_SPIF:
+    in r18, SPSR
+    sbrs r18, 7
+    rjmp Wait_SPIF
+
+    in r24, SPDR
+    sts 0x0203, r24
+    lds r24, 0x0203
+    andi r24, 0xc0
+    sts 0x0202, r24
+
+    ldi r25, 0x00
+    cpi r24, 0x40
+    cpc r25, r1;
+    ;spi data == 0x40
+    brbs 1, Subroutine_TX_Frame
+
+    ;else
+    out sreg, r1
+    sbiw r24, 0x00
+    ;spi data == 0x20
+    brbs 1, Subroutine_RX_Frame
+
+    out sreg,r1
+    cpi r24, 0x80
+    cpc r25, r1;
+    brbs 1, Subroutine_Rd_Reg
+
+    out sreg,r1
+    cpi r24, 0xC0
+    cpc r25, r1;
+    brbs 1, Subroutine_Wr_Reg
+
+    rjmp Wait_SPIF
+
+    Subroutine_Rd_Reg:
+    lds r24, 0x0203
+    andi r24, 0x3f
+    ldi r25, 0x00
+    sts 0x0201, r25
+    sts 0x0200, r24
+    subi r24, 0xc0
+    sbci r25, 0xfe
+    sts 0x0201, r25
+    sts 0x0200, r24
+    rcall SPI_TX_Reg
+    rjmp Wait_SPIF
+
+    Subroutine_Wr_Reg:
+    lds r24, 0x0203
+    andi r24, 0x3f
+    ldi r25, 0x00
+    sts 0x0201, r25
+    sts 0x0200, r24
+    subi r24, 0xc0
+    sbci r25, 0xfe
+    sts 0x0201, r25
+    sts 0x0200, r24
+    rcall SPI_RX_Reg
+    rjmp Wait_SPIF
+
+    
+
 
--- src/Mega128_status_trx.asm
+++ src/Mega128_status_trx.asm
@@ -1,1 +1,80 @@
-.org 0x200
+SPI_TX_Reg:
+    push r28
+    push r29
+    push r1
+    in r28, SPL
+    in r29, SPH
+    lds r24, 0x0200
+    lds r25, 0x0201
+    cpi r24, 0x4f
+    ldi r18, 0x01
+    cpc r25, r18
+    brbc 1, ReadInterrupt
+    ;else
+    ldi r24, 0x4e
+    ldi r25, 0x00
+    lds r18, 0x0200
+    lds r19, 0x0201
+    movw r30, r18
+    ld r18, Z
+    movw r30, r24
+    st Z, r18
+    rjmp Wait_RdEnd
+    
+
+    ReadInterrupt:
+    ldi r24, 0x4e
+    ldi r25, 0x00
+    lds r18, 0x0205
+    movw r30, r24
+    st Z, r18
+    in r24, SREG
+    std Y+1, r24;PUSH SREG
+    bclr 7
+    sts 0x0205, r1
+    ldi r24, 0x5F
+    ldi r25, 0x00
+    movw Z,r24
+    ldd r18, Y+1
+    out SREG, r18
+    
+    Wait_RdEnd:
+    in r24 SPSR
+    and r24, r24
+    and r24, Wait_RdEnd
+    ld r24, SPDR
+    pop r0
+    pop r29
+    pop r28
+    ret
+
+
+SPI_RX_Reg:
+    push r28
+    push r29
+    push r1
+    Wait_WrEnd:
+    in r28, SPL
+    in r29, SPH
+    in r24, SPSR
+    and r24, r24
+    brbc 4, Wait_WrEnd
+    in r24, SPDR
+    sts 0x0203, r24
+
+    lds r24, 0x0200
+    lds r25, 0x0201
+    cpi r24, 0x42
+    ldi r31, 0x01
+    cpc r25, r31
+    ;if writing 142
+    brbc 1, WriteStatus
+
+    lds r24, 0x0203
+    andi r24, 0x1f
+    std Y+1, r24
+    ldd r24, Y+1
+    cpi r24, 0x16
+  
+
+    WriteStatus:
